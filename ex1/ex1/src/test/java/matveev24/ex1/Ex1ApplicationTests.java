5f28c39493f6e24f3764fe0a2fb3a9a54cc6bd35
matveevfg <matveevfg@mail.ru>
2023-12-17 15:45:29 +0500

    Create

---
+++ ex1/ex1/src/test/java/matveev24/ex1/Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package matveev24.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
